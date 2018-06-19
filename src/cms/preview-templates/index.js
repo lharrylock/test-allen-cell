82aae697d77ab2a63f2d765607b08dcae09e9e19
Lisa Harrylock <lisah@alleninstitute.org>
2018-06-19 10:23:40 -0700

    export all templates

---
+++ src/cms/preview-templates/index.js
@@ -0,0 +1,11 @@
+import AboutPagePreview from './AboutPagePreview';
+import BlogPostPreview from './BlogPostPreview';
+import CustomPagePreview from './CustomPagePreview';
+import ProductPagePreview from './ProductPagePreview';
+
+export {
+  AboutPagePreview,
+  BlogPostPreview,
+  CustomPagePreview,
+  ProductPagePreview,
+}
