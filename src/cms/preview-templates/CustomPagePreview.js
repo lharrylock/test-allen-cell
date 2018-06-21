36eec059e18f1567d14ec0fc6a6060c78db6fc41
Lisa Harrylock <lisah@alleninstitute.org>
2018-06-21 07:49:07 -0700

    add wip

--- src/cms/preview-templates/CustomPagePreview.js
+++ src/cms/preview-templates/CustomPagePreview.js
@@ -2,16 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CustomPageTemplate } from '../../templates/custom-page';
 
-const CustomPagePreview = ({ entry, getAsset, widgetFor }) => {
+const CustomPagePreview = ({ entry, getAsset, widgetsFor, widgetFor }) => {
   const chunks = entry.getIn(['data', 'chunk']);
-  return (
-    <CustomPageTemplate
-      title={entry.getIn(['data', 'title'])}
-      chunks={chunks ? chunks.toJS() : []}
-      widgetFor={widgetFor}
-      getAsset={getAsset}
-    />
-  )
+  let widgets = widgetsFor('chunk')
+  if (widgets) {
+    let i = 0;
+    widgets = widgets.map(w => {
+      i++;
+
+      return (<div>
+        {w}
+      </div>);
+    });
+    console.log(widgets.toJSON())
+  }
+  //
+  //  console.log(widgets ? widgets.map(chunk => chunk ? chunk.getIn(['data', 'image']) : 'nothing') : "test")
+  // console.log(widgets ? widgets.map(chunk => chunk ? chunk.getIn(['data', 'image']) : 'nothing') : "test")
+  // return (
+  //   <CustomPageTemplate
+  //     title={entry.getIn(['data', 'title'])}
+  //     chunks={chunks ? chunks.toJS() : []}
+  //     widgetFor={widgetsFor}
+  //     getAsset={getAsset}
+  //   />
+  // )
+  return widgets || <div>Nothing to show</div>
+  // return widgetFor('title')
 }
 
 
