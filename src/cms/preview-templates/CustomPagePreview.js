0a3e2f16c4ce1d2efbab9d089a5922d5168a1e6b
lisajoy <lisaharrylock@gmail.com>
2018-06-21 10:40:00 -0700

    custom page preview

--- src/cms/preview-templates/CustomPagePreview.js
+++ src/cms/preview-templates/CustomPagePreview.js
@@ -1,42 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ImageAndCaptionPreview from "./ImageAndCaptionPreview";
+
 import { CustomPageTemplate } from '../../templates/custom-page';
 
-const CustomPagePreview = ({ entry, getAsset, widgetsFor, widgetFor }) => {
-  const chunks = entry.getIn(['data', 'chunk']);
-  let widgets = widgetsFor('chunk')
-  if (widgets) {
-    let i = 0;
-    widgets = widgets.map(w => {
-      i++;
+const objectPreviewMap = new Map([
+    ['imageAndCaption', ImageAndCaptionPreview]
+]);
 
-      return (<div>
-        {w}
-      </div>);
+const CustomPagePreview = ({ entry, getAsset, widgetsFor }) => {
+  let widgets = widgetsFor('chunk');
+  if (widgets) {
+    widgets = widgets
+        .filter(w => !!w)
+        .map((w, i) => {
+            let Preview, props;
+            const widgetName = w.getIn(['data', 'widget']) || '?';
+            const widget = CMS.getWidget(widgetName);
+            const name = w.getIn(['data', 'name']) || '?';
+
+            if (widget && widgetName !== 'object') {
+                Preview = widget.preview;
+                props = {value: w.getIn(['data', widgetName])};
+            } else {
+                Preview = objectPreviewMap.get(name);
+                props = w.getIn(['data', name]);
+                props = props ? props.toJS() : null;
+            }
+            props = {
+                ...props,
+                getAsset
+            };
+
+            return Preview ? (
+                <Preview {...props} key={i} />
+            ) : <div>{`Warning: no preview component registered for widget of type: ${widgetName}, name: ${name}`}</div>;
     });
-    console.log(widgets.toJSON())
   }
-  //
-  //  console.log(widgets ? widgets.map(chunk => chunk ? chunk.getIn(['data', 'image']) : 'nothing') : "test")
-  // console.log(widgets ? widgets.map(chunk => chunk ? chunk.getIn(['data', 'image']) : 'nothing') : "test")
-  // return (
-  //   <CustomPageTemplate
-  //     title={entry.getIn(['data', 'title'])}
-  //     chunks={chunks ? chunks.toJS() : []}
-  //     widgetFor={widgetsFor}
-  //     getAsset={getAsset}
-  //   />
-  // )
-  return widgets || <div>Nothing to show</div>
-  // return widgetFor('title')
-}
+  return (
+    <CustomPageTemplate
+      title={entry.getIn(['data', 'title'] || '')}
+      chunks={widgets || []}
+    />
+  )
+};
 
 
 CustomPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  widgetFor: PropTypes.func,
+  widgetsFor: PropTypes.func,
   getAsset: PropTypes.func,
 }
 
